f33ea5b5db8c077f89503e3eb6097b17e7c8cecb
Michaela Meissner <m.p.meissner85@gmail.com>
2023-03-17 16:38:43 -0400

    Created ReplySerializer & CommentSerializer

---
+++ backend/replies/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Reply
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Reply
+        fields = ['user_id', 'comment', 'text']
+        depth = 1
