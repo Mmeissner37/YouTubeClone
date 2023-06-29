bac7c203e613b45c996e4cbb075c7c957ed555fd
Michaela Meissner <m.p.meissner85@gmail.com>
2023-06-29 19:37:26 -0400

    Added Link Feature to Related Videos

--- frontend/src/components/VideoMapper.jsx
+++ frontend/src/components/VideoMapper.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import VideoPresenter from "./VideoPresenter";
+import { Link } from "react-router-dom";
 
-
-const VideoMapper = ({videoArray}) => {
+const VideoMapper = ({videoArray, setVideos}) => {
     return (
         <div className="video-flex">
-            {videoArray.map(el => <VideoPresenter key={el.id.videoId} video = {el} />)}
+            {videoArray.map(el => <Link to={`/video/${el.id.videoId}`} onClick ={() => setVideos(el.id.videoId)}>
+                <VideoPresenter key={el.id.videoId} video = {el} />
+                </Link>)}
         </div>
 
     )
--- frontend/src/components/VideoPresenter.jsx
+++ frontend/src/components/VideoPresenter.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const VideoPresenter = ({video}) => {
     return ( 
     <div className='vidpresenter'>
-        <img src={video.snippet.thumbnails.default.url} height={video.snippet.thumbnails.default.height} width={video.snippet.thumbnails.default.width} alt=""/>
+        <img src={video.snippet.thumbnails.default.url} alt=""/>
         <h4>{video.snippet.title}</h4>
     </div> );
 }
